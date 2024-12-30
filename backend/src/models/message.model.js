719c1985c88ca8e003b8f5121f74b9bcd3d5d11a
Shubham Raj <shubhamraj963133@gmail.com>
2024-12-30 12:37:06 +0530

    commit

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }  //its show time ex-12:10PM just like that
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
